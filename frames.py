5e9fd6ddcb36d462c84a9839949d8deb1e357f16
Gaia R <42282250+grajan22@users.noreply.github.com>
2020-11-01 12:29:35 -0500

    Get average BGR values and isolate frame by frame

--- frames.py
+++ frames.py
@@ -1,9 +1,10 @@
-from PIL import ImageSequence
-
-gif = Image.open("object.gif")
-
-oldFrame = gif.seek(0) #get first frame as oldFrame to start with
-for frame in ImageSequence.Iterator(gif):
-    newFrame = frame
-    # add analysis code here
-    oldFrame = frame #update oldFrame
+from PIL import Image, ImageSequence
+import cv2
+import numpy
+from PIL import Image, ImageSequence
+img = Image.open("object.gif")
+for i, frame in enumerate(ImageSequence.Iterator(img)):
+    dst = frame.convert("RGB")
+    avg_color_per_row = numpy.average(dst, axis=0)
+    avg_color = numpy.average(avg_color_per_row, axis=0)
+    print(avg_color) #BGR order
